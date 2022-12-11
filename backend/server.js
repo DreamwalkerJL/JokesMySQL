73f0ca067934fd5865baf44da4be28c0cb507297
Joshua Lim <joshua.lim@hotmail.ch>
2022-12-11 15:05:48 +0100

    cleanup

--- backend/server.js
+++ backend/server.js
@@ -70,6 +70,10 @@ app.put("/jokes/:id", (req, res) => {
 });
 
 // Serve frontend
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
